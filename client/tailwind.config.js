5cabf4d90f76105cc6875459bf6647a7e679e486
anishdass <anishdassatoffice@gmail.com>
2025-09-10 17:20:06 +0100

    Initial commit; Working on Navbar

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#28A745",
+        secondary: "#1F3A93",
+      },
+    },
+  },
+  plugins: [],
+};
